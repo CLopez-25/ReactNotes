d32a138d291a8f2a875262770b29c35de3cf71da
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-17 16:54:42 -0500

    adding notes for react tutorial

--- src/index.js
+++ src/index.js
@@ -16,3 +16,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+                                // REACT STARTUP PROCESS ///
+/*
+    1. YOU RUN THIS LINE OF CODE: const rootElement = document.getElementById('root');
+THIS LINE OF CODE GETS A REFERENCE TO A DIV WITH THE ID OF ROOT.
+
+    2. YOU RUN THIS LINE OF CODE: const roo = createRoot(rootElement);
+THIS TELLS REACT THAT YOU WANT TO TAKE CONTROL OF THAT DIV AND RENDER OUR APP TO IT.
+
+    3. YOU ARE TELLING REACT THAT YOU WANT TO RENDER YOUR APP COMPONENTS AND SHOW ALL JSX, TURN IT INTO HTML, AND SHOW IT IN THE ROOT DIV.
+
+
+
+
+ */
