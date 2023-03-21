50a5bc857adecb053bb284d649704182c5f1d2d1
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-21 14:31:44 -0500

    setting up for react tutorial project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // 1) Import React and ReactDOM libraries
+import React from 'react';                              // React is the library that defines what a component is and how multiple components work together
+import ReactDOM from 'react-dom/client';                // ReactDOM is the library that knows how to get a component to  show up in the browser
 
 // 2) Get a reference to the div with ID root
+
 
 // 3) Tell React to take control of that element
 
