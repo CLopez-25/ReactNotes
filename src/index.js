f74968264f6d707dce1a5971f514c8b5465059e5
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-21 20:13:00 -0500

    adding comments/code for react tutorial

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import React from 'react';                              // React is the library 
 import ReactDOM from 'react-dom/client';                // ReactDOM is the library that knows how to get a component to  show up in the browser
 
 // 2) Get a reference to the div with ID root
-const el = document.getElementById('root');            // el is a reference to the div with ID root
+const el = document.getElementById('root');            // el (element) is a reference to the div with ID root
 
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);                  // root is a reference to the div with ID root
 
 // 4) Create a react component
-function App() {
+function App() {                                     // App is a function that returns JSX
     return <h1>Hi there!</h1>
 }
 
 // 5) Show the component on the screen
-root.render(<App />);
+root.render(<App />);                              // root.render() takes a component and renders it to the screen
