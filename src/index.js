1ea625eaa6d05f0e2cd884b2573952e3b4679694
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-22 16:29:47 -0500

    adding notes for react tutorial

--- src/index.js
+++ src/index.js
@@ -9,9 +9,16 @@ const el = document.getElementById('root');            // el (element) is a refe
 const root = ReactDOM.createRoot(el);                  // root is a reference to the div with ID root
 
 // 4) Create a react component
-function App() {                                     // App is a function that returns JSX
-    return <h1>Hi there!</h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
-}                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
+// function App() {                                     // App is a function that returns JSX
+//     return <h1>Hi there!</h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
+// }                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
+function App() {
+    let message = 'Bye there!'
+    if (Math.random() > 0.5) {
+        message = 'Hi there!';
+    }
+    return <h1>{message}</h1>                       // JSX is a mix of JS and HTML. We can use JS expressions inside of JSX by wrapping them in curly braces. This is called 'interpolation
+}
 
 // 5) Show the component on the screen
 root.render(<App />);                              // root.render() takes a component and renders it to the screen
