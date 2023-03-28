e046df3d54a378f57bf0aa07a433814c113ad771
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-27 20:12:17 -0500

    continuing react tutorial

--- src/index.js
+++ src/index.js
@@ -9,16 +9,22 @@ const el = document.getElementById('root');            // el (element) is a refe
 const root = ReactDOM.createRoot(el);                  // root is a reference to the div with ID root
 
 // 4) Create a React component
-// function App() {                                     // App is a function that returns JSX
-//     return <h1>Hi there!</h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
-// }                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
-function App() {
-    let message = 'Bye there!'
-    if (Math.random() > 0.5) {                      // Math.random() returns a random number between 0 and 1. If the number is greater than 0.5, the message will be 'Hi there!'
-        message = 'Hi there!';
-    }
-    return <h1>{message}</h1>                       // JSX is a mix of JS and HTML. We can use JS expressions inside JSX by wrapping them in curly braces. This is called 'interpolation
-}
+function App() {                                     // App is a function that returns JSX
+    const name = 'Cesar';
+    const age = 27;
+
+    return <h1>Hi there!
+        My name is {name} and my age is {age}!
+           </h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
+}                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
+
+// function App() {
+//     let message = 'Bye there!'
+//     if (Math.random() > 0.5) {                      // Math.random() returns a random number between 0 and 1. If the number is greater than 0.5, the message will be 'Hi there!'
+//         message = 'Hi there!';
+//     }
+//     return <h1>{message}</h1>                       // JSX is a mix of JS and HTML. We can use JS expressions inside JSX by wrapping them in curly braces. This is called 'interpolation
+// }
 
 // 5) Show the component on the screen
 root.render(<App />);                              // root.render() takes a component and renders it to the screen
