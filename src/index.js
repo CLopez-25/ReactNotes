2d7450376d2fc3820f5bb749051155b2ba75c4bd
Clopez1725 <lopez.m.cesar17@gmail.com>
2023-03-28 19:21:33 -0500

    modified files and added notes from react tutorial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // 1) Import React and ReactDOM libraries
 import React from 'react';                              // React is the library that defines what a component is and how multiple components work together
 import ReactDOM from 'react-dom/client';                // ReactDOM is the library that knows how to get a component to  show up in the browser
+import App from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');            // el (element) is a reference to the div with ID root
@@ -9,14 +10,14 @@ const el = document.getElementById('root');            // el (element) is a refe
 const root = ReactDOM.createRoot(el);                  // root is a reference to the div with ID root
 
 // 4) Create a React component
-function App() {                                     // App is a function that returns JSX
-    const name = 'Cesar';
-    const age = 27;
-
-    return <h1>Hi there!
-        My name is {name} and my age is {age}!
-           </h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
-}                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
+// function App() {                                     // App is a function that returns JSX
+//     const name = 'Cesar';
+//     const age = 27;
+//
+//     return <h1>Hi there!
+//         My name is {name} and my age is {age}!
+//            </h1>                       // WRITING THIS <h1>Hi there!</h1> DOESNT ACTUALLY MAKE ANYTHING APPEAR ON THE BROWSER AUTOMATICALLY
+// }                                                   // THIS JUST CREATES AN INSTRUCTION FOR REACT, TELLING IT TO MAKE AN ELEMENT. WE HAVE TO RETURN IT FROM A COMPONENT IN ORDER FOR REACT TO USE IT.
 
 // function App() {
 //     let message = 'Bye there!'
